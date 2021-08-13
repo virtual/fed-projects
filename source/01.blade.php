aa0cd47ddd3e64d24dee835efa824f608f683f45
Jeanine Schoessler <jeanine.mt@gmail.com>
2021-08-12 18:35:44 -0600

    rebuild using default vars

--- source/01.blade.php
+++ source/01.blade.php
@@ -1,47 +1,47 @@
 @extends('_layouts.main')
 
 @section('body')
+<div class="py-5">
+  <section class="card-container">
+    <div class="container">
+      <div class="row">
+        <div class="card col-16">
+          <h1>Join our community</h1>
 
-<section class="card-container">
-  <div class="container">
-    <div class="row">
-      <div class="card col-16">
-        <h1>Join our community</h1>
+          <p class="lead color-yellow">30-day, hassle-free money back guarantee</p>
 
-        <p class="lead color-yellow">30-day, hassle-free money back guarantee</p>
+          <p class="lead color-grey">Gain access to our full library of tutorials along with expert code reviews. 
+          Perfect for any developers who are serious about honing their skills.</p>
+          
+        </div>
+      </div>
+      <div class="row">
+        <div class="card col-8 theme-primary theme-inversed">
+          <h2>Monthly Subscription</h2>
 
-        <p class="lead color-grey">Gain access to our full library of tutorials along with expert code reviews. 
-        Perfect for any developers who are serious about honing their skills.</p>
-        
+          <p class="lead"><span class="type-feature">&dollar;29</span> <span class="type-dimmed">per month</span></p>
+
+          <p class="lead">Full access for less than &dollar;1 a day</p>
+
+          <p class="mt-5"><a href="#" class="btn btn-green">Sign Up</a></p>
+        </div>
+
+        <div class="card col-8 theme-secondary theme-inversed">
+          <h2>Why Us</h2>
+          <ul class="list-unstyled">
+          <li>Tutorials by industry experts</li>
+          <li>Peer &amp; expert code review</li>
+          <li>Coding exercises</li>
+          <li>Access to our GitHub repos</li>
+          <li>Community forum</li>
+          <li>Flashcard decks</li>
+          <li>New videos every week</li>
+          </ul>
+        </div>
       </div>
     </div>
-    <div class="row">
-      <div class="card col-8 theme-primary">
-        <h2>Monthly Subscription</h2>
-
-        <p class="lead"><span class="type-feature">&dollar;29</span> <span class="type-dimmed">per month</span></p>
-
-        <p class="lead">Full access for less than &dollar;1 a day</p>
-
-        <p class="mt-5"><a href="#" class="btn btn-green">Sign Up</a></p>
-      </div>
-
-      <div class="card col-8 theme-secondary">
-        <h2>Why Us</h2>
-        <ul class="list-unstyled">
-        <li>Tutorials by industry experts</li>
-        <li>Peer &amp; expert code review</li>
-        <li>Coding exercises</li>
-        <li>Access to our GitHub repos</li>
-        <li>Community forum</li>
-        <li>Flashcard decks</li>
-        <li>New videos every week</li>
-        </ul>
-      </div>
-    </div>
-  </div>
-</section>
- 
+  </section>
+</div>
 @endsection
 
 @section('head')
